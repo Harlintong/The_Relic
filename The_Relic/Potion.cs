6a38432e308c8fff27b6eb248b7437fa224fd114
Harlintong Urbina Bermudez <harlintonggeonieth@learnet.se>
2019-11-13 10:02:52 +0100

    starting to undestand classes

---
+++ The_Relic/Potion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Relic
+{
+    class Potion : Item
+    {
+        private int heal = 20;
+
+        public void Use(Hero target)
+        {
+            target.ModifyHp(heal);
+            Console.WriteLine("");
+        }
+    }
+}
